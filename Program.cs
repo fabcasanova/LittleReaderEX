6a88ab9a93fc84bebef1356fdf82e94336b4c889
Fabian_C <54602317+fabcasanova@users.noreply.github.com>
2021-11-12 17:17:21 -0800

    stuff added in folder for reference

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MemoryStreamReaderLastLineAlgo
+{
+    class Program
+    {
+        private static string file = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "testData.txt");
+        static void Main(string[] args)
+        {
+            readInputFromFile();
+        }
+
+        private static void readInputFromFile() 
+        {
+            using (FileStream fs = File.OpenRead(file))
+            {
+                using (StreamReader sw = new StreamReader(fs))
+                {
+                    long end = fs.Length;
+                    fs.Seek(-82, SeekOrigin.End);
+
+                    Console.WriteLine(sw.ReadLine());
+
+                }
+            }
+        }
+    }
+}
